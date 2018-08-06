e59bb17318a716be55610ea33422e2c18a206e68
dmitriyHavrilovich <dhavrilovich@cybervisiontech.com>
2018-08-06 18:38:15 +0300

    added utils for message formatting

---
+++ src/main/java/com/mapr/fuse/utils/MessageUtils.java
@@ -0,0 +1,21 @@
+package com.mapr.fuse.utils;
+
+import com.mapr.fuse.dto.MessageConfig;
+
+public class MessageUtils {
+    public static int getSeparatorsLength(MessageConfig messageConfig) {
+        return (messageConfig.getSeparator() + messageConfig.getStart() + messageConfig.getStop()).getBytes().length;
+    }
+
+    public static String formatMessage(MessageConfig messageConfig, String message, boolean cutStart) {
+        if(cutStart)
+            return String.format("%s%s%s", message, messageConfig.getStop(), messageConfig.getSeparator());
+        else if(messageConfig.getSize())
+            return String.format("%s%d%s%s%s", messageConfig.getStart(), message.length(), message,
+                    messageConfig.getStop(), messageConfig.getSeparator());
+        else
+            return String.format("%s%s%s%s", messageConfig.getStart(), message, messageConfig.getStop(),
+                    messageConfig.getSeparator());
+
+    }
+}
