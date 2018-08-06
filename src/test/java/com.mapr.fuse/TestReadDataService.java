d19687cec16c15f8dd1d90211ab9650dfab9f60e
dmitriyHavrilovich <dhavrilovich@cybervisiontech.com>
2018-08-06 18:41:41 +0300

    added tests for cutoff

--- src/test/java/com.mapr.fuse/TestReadDataService.java
+++ src/test/java/com.mapr.fuse/TestReadDataService.java
@@ -3,6 +3,7 @@ package com.mapr.fuse;
 import com.mapr.fuse.client.TopicReader;
 import com.mapr.fuse.dto.MessageConfig;
 import com.mapr.fuse.service.ReadDataService;
+import com.mapr.fuse.utils.MessageUtils;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.utils.Bytes;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -18,6 +19,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -37,17 +39,30 @@ public class TestReadDataService {
 
     public final static int MIN_MESSAGE_LENGTH = 5;
     public final static int MAX_MESSAGE_LENGTH = 20;
-    public final static int MESSAGE_AMOUNT = 7;
+    public final static int MESSAGE_AMOUNT = 10;
+    public final static int START_CUTOFF = 3;
+    public final static int MESSAGE_LENGTH = 30;
+
+    public final static String TOPIC_NAME = "test_topic";
+    public final static int PARTITION = 1;
+    public List<String> messageList;
 
     @Before
     public void init() throws Exception {
-        messageConfig = new MessageConfig("START", "END", "|", false);
+        messageConfig = new MessageConfig("START", "END", "|", true);
+        messageList = getRandomMessages(MESSAGE_AMOUNT, MIN_MESSAGE_LENGTH, MAX_MESSAGE_LENGTH);
 
         byte[] configArray = new ObjectMapper().writeValueAsBytes(messageConfig);
         Stream<Bytes> configStream = Stream.of(new Bytes(configArray));
 
         when(topicReader.readPartition(ArgumentMatchers.eq(new TopicPartition("/fuse_config:message_config", 0)),
                 anyLong(), anyLong())).thenReturn(configStream);
+
+        when(topicReader.readPartition(ArgumentMatchers.eq(new TopicPartition(TOPIC_NAME, PARTITION)),
+                anyLong(), anyLong(), anyLong())).thenReturn(Stream.of(convertMessagesToBytes(messageList)));
+
+        when(topicReader.readPartition(ArgumentMatchers.eq(new TopicPartition(TOPIC_NAME, PARTITION)),
+                anyLong(), anyLong(), eq(1000L))).thenReturn(Stream.of(convertMessagesToBytes(messageList)));
     }
 
     @Test
@@ -62,39 +77,76 @@ public class TestReadDataService {
 
     @Test
     public void testRequestTopicSizeData() {
-        String msg = getRandomString(MIN_MESSAGE_LENGTH, MAX_MESSAGE_LENGTH);
-        String topic = "test_topic";
-        int partition = 1;
+        String resultMessage = messageList.stream().map(msg ->
+                MessageUtils.formatMessage(messageConfig, msg, false))
+                .collect(Collectors.joining());
 
-        int separatorsLength = getSeparatorsLength();
-
-        when(topicReader.readPartition(ArgumentMatchers.eq(new TopicPartition(topic, partition)),
-                anyLong(), anyLong(), anyLong())).thenReturn(Stream.of(new Bytes(msg.getBytes())));
-
-        Assert.assertEquals(Integer.valueOf(msg.length() + separatorsLength), readDataService.requestTopicSizeData(topic, partition));
+        Assert.assertEquals(Integer.valueOf(resultMessage.getBytes().length),
+                readDataService.requestTopicSizeData(TOPIC_NAME, PARTITION));
     }
 
     @Test
     public void testReadRequiredBytesFromTopicPartition() {
-        String topic = "test_topic";
-        int partition = 1;
-        List<String> messageList = getRandomMessages(MESSAGE_AMOUNT, MIN_MESSAGE_LENGTH, MAX_MESSAGE_LENGTH);
+        readDataService.requestTopicSizeData(TOPIC_NAME, PARTITION);
 
-        when(topicReader.readPartition(ArgumentMatchers.eq(new TopicPartition(topic, partition)),
-                anyLong(), anyLong(), anyLong())).thenReturn(Stream.of(convertMessagesToBytes(messageList)));
-
-        when(topicReader.readPartition(ArgumentMatchers.eq(new TopicPartition(topic, partition)),
-                anyLong(), anyLong(), eq(1000L))).thenReturn(Stream.of(convertMessagesToBytes(messageList)));
-
-        readDataService.requestTopicSizeData(topic, partition);
-
-        String resultMessage = messageList.stream().map(msg -> formatMessage(messageConfig, msg))
+        String resultMessage = messageList.stream().map(msg ->
+                MessageUtils.formatMessage(messageConfig, msg, false))
                 .collect(Collectors.joining());
 
         String msgFromService = new String(readDataService.readRequiredBytesFromTopicPartition(
-                new TopicPartition(topic, partition), 0L, 1000L, 1000L));
+                new TopicPartition(TOPIC_NAME, PARTITION), 0L, (long) resultMessage.length(), 1000L));
 
         Assert.assertEquals(resultMessage, msgFromService);
+    }
+
+    @Test
+    public void testReadRequiredBytesFromTopicPartitionWithStartCutoff() {
+        readDataService.requestTopicSizeData(TOPIC_NAME, PARTITION);
+
+        AtomicInteger index = new AtomicInteger();
+
+        String resultMessage = messageList.stream().map(msg ->
+                index.getAndIncrement() == 0 ?
+                    MessageUtils.formatMessage(messageConfig, msg.substring(START_CUTOFF), true) :
+                    MessageUtils.formatMessage(messageConfig, msg, false))
+                .collect(Collectors.joining());
+
+        String msgFromService = new String(readDataService.readRequiredBytesFromTopicPartition(
+                new TopicPartition(TOPIC_NAME, PARTITION), (long) START_CUTOFF, (long) resultMessage.length() , 1000L));
+
+        Assert.assertEquals(resultMessage, msgFromService);
+    }
+
+    @Test
+    public void testReadRequiredBytesFromTopicPartitionWithEndCutoff() {
+        readDataService.requestTopicSizeData(TOPIC_NAME, PARTITION);
+
+        String resultMessage = messageList.stream().map(msg ->
+                MessageUtils.formatMessage(messageConfig, msg, false))
+                .collect(Collectors.joining());
+
+        String msgFromService = new String(readDataService.readRequiredBytesFromTopicPartition(
+                new TopicPartition(TOPIC_NAME, PARTITION), 0L, (long) MESSAGE_LENGTH, 1000L));
+
+        Assert.assertEquals(new String(Arrays.copyOfRange(resultMessage.getBytes(), 0 , MESSAGE_LENGTH)), msgFromService);
+    }
+
+    @Test
+    public void testReadRequiredBytesFromTopicPartitionWithStartAndEndCutoff() {
+        readDataService.requestTopicSizeData(TOPIC_NAME, PARTITION);
+
+        AtomicInteger index = new AtomicInteger();
+
+        String resultMessage = messageList.stream().map(msg ->
+                index.getAndIncrement() == 0 ?
+                        MessageUtils.formatMessage(messageConfig, msg.substring(START_CUTOFF), true) :
+                        MessageUtils.formatMessage(messageConfig, msg, false))
+                .collect(Collectors.joining());
+
+        String msgFromService = new String(readDataService.readRequiredBytesFromTopicPartition(
+                new TopicPartition(TOPIC_NAME, PARTITION), (long) START_CUTOFF, (long) MESSAGE_LENGTH, 1000L));
+
+        Assert.assertEquals(new String(Arrays.copyOfRange(resultMessage.getBytes(), 0 , MESSAGE_LENGTH)), msgFromService);
     }
 
     public Bytes[] convertMessagesToBytes(List<String> messageList) {
@@ -113,26 +165,19 @@ public class TestReadDataService {
     public String getRandomString(int minLength, int maxLength) {
         int leftLimit = 48;
         int rightLimit = 122;
-        int targetStringLength = ThreadLocalRandom.current().nextInt(minLength, maxLength + 1);
+        int targetStringLength = ThreadLocalRandom.current().nextInt(minLength, maxLength - 1);
 
         Random random = new Random();
 
         StringBuilder buffer = new StringBuilder(targetStringLength);
+        buffer.append("{");
         for (int i = 0; i < targetStringLength; i++) {
             int randomLimitedInt = leftLimit + (int)
                     (random.nextFloat() * (rightLimit - leftLimit + 1));
             buffer.append((char) randomLimitedInt);
         }
+        buffer.append("}");
 
        return buffer.toString();
-    }
-
-    public int getSeparatorsLength() {
-        return (messageConfig.getSeparator() + messageConfig.getStart() + messageConfig.getStop()).getBytes().length;
-    }
-
-    private String formatMessage(MessageConfig messageConfig, String message) {
-        return String.format("%s%s%s%s",
-                messageConfig.getStart(), message, messageConfig.getStop(), messageConfig.getSeparator());
     }
 }
